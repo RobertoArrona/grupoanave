e056ad7848e46e650de64daa58357b7fd0305815
cristian100 <cristian100@gmail.com>
2015-08-02 20:58:11 -0600

    added styles to solicitar cotizacion form

---
+++ sites/all/themes/grupoanave/js/cotizacion.block.js
@@ -0,0 +1,14 @@
+(function ($) {
+  Drupal.behaviors.cotizacionBlock = {
+   attach: function (context, settings) {
+     $('#block-webform-client-block-82', context).once('toggle', function () {
+        $(this).find('> h2.block-title').append('<span class="toggle-button"></span>');
+        $(this).find('> h2.block-title span.toggle-button').click(function(event){
+          block = $(this).parent().parent();
+          $(block).toggleClass('closed');
+          (event.preventDefault) ? event.preventDefault() : event.returnValue = false;
+        });
+      });
+    }
+  };
+})(jQuery);
