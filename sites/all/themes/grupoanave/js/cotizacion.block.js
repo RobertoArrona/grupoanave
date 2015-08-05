3817b39b0da60d3d76cb16a97cce882490f2b7f4
cristian100 <cristian100@gmail.com>
2015-08-04 21:14:04 -0600

    added logic for cookie

--- sites/all/themes/grupoanave/js/cotizacion.block.js
+++ sites/all/themes/grupoanave/js/cotizacion.block.js
@@ -2,18 +2,29 @@
   /**
    * Vars
    */
-  var blockClosed = $.cookie('blockClosed', '0', { expires: 7, path: '/' });
-  $.cookie('blockClosed', '0', { expires: 7, path: '/' });
+  if (typeof $.cookie('blockClosed') === 'undefined') {
+    $.cookie('blockClosed', '0', { expires: 7, path: '/' });
+  }
   
   Drupal.behaviors.cotizacionBlock = {
     attach: function (context, settings) {
       $('#block-webform-client-block-87', context).once('toggle', function () {
-        alert($.cookie('blockClosed'));
+        if ( $.cookie('blockClosed') == '1' ) {
+          $(this).addClass('closed');
+        }
+        
         $(this).find('> h2.block-title').append('<span class="toggle-button"></span>');
         $(this).find('> h2.block-title span.toggle-button').click(function(event){
           block = $(this).parent().parent();
-          $(block).toggleClass('closed');
-          $.cookie('blockClosed', '1');
+          if ( $.cookie('blockClosed') == '0' ) {
+            $(block).addClass('closed');
+            $.cookie('blockClosed', '1');
+          } else {
+            $.cookie('blockClosed', '0');
+            $(block).removeClass('closed');
+          }
+          
+          
           (event.preventDefault) ? event.preventDefault() : event.returnValue = false;
         });
       });
