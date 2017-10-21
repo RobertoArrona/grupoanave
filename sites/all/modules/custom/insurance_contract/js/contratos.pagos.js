aea169a105b1f3b141bfd409036888f17997e20c
Jorge Leonel Sánchez Fregozo <jorgeleonelsanchezfregozo@Jorges-MacBook-Pro.local>
2017-10-21 13:23:49 -0600

    feature/302: Add relation from the payment field to payment subsequent field.

---
+++ sites/all/modules/custom/insurance_contract/js/contratos.pagos.js
@@ -0,0 +1,32 @@
+(function ($) {
+  $(document).on('ready', function(){
+    var selector =  $('#edit-field-poliza-forma-pago select');
+    var label =  $('#edit-field-poliza-primas-recibos-subs label');
+    if (selector.length > 0) {
+      if (selector.val() != '_none' && selector.val() !='anual'){
+        updateLabel(label,selector.val());
+      }
+    }
+    selector.change(function(){
+      if (selector.length > 0) {
+        if (selector.val() != '_none' && selector.val() !='anual'){
+          updateLabel(label,selector.val());
+        }
+      }
+    });
+  });
+  function updateLabel(label, text) {
+    label.text(getPaymentsLabel(text));
+  }
+  function getPaymentsLabel(period){
+    if (period == 'mensual') {
+      return 'Primas Recibos Subs:(11) ';
+    }
+    else if(period == 'trimestral') {
+      return 'Primas Recibos Subs:(3) ';
+    }
+    else if(period == 'semestral') {
+      return 'Primas Recibos Subs:(1) ';
+    }
+  }
+})(jQuery);
