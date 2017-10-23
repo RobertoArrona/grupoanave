d4fb5c24bdc7e90c9527a611da429645f5983ac2
jcastrod4all <jcastro@d4all.mx>
2017-10-23 12:39:42 -0600

    feature/364: fixed the contract number label using .hide jQuery function

--- sites/all/modules/custom/insurance_contract/js/contratos.pagos.js
+++ sites/all/modules/custom/insurance_contract/js/contratos.pagos.js
@@ -1,32 +1,34 @@
 (function ($) {
-  $(document).on('ready', function(){
-    var selector =  $('#edit-field-poliza-forma-pago select');
-    var label =  $('#edit-field-poliza-primas-recibos-subs label');
-    if (selector.length > 0) {
-      if (selector.val() != '_none' && selector.val() !='anual'){
-        updateLabel(label,selector.val());
-      }
-    }
-    selector.change(function(){
+    $(document).on('ready', function(){
+      var selector =  $('#edit-field-poliza-forma-pago select');
+      var label =  $('#edit-field-poliza-primas-recibos-subs label');
+      var labeltitle  = $('label[for="edit-title"]').hide();
+      
       if (selector.length > 0) {
         if (selector.val() != '_none' && selector.val() !='anual'){
           updateLabel(label,selector.val());
         }
       }
+      selector.change(function(){
+        if (selector.length > 0) {
+          if (selector.val() != '_none' && selector.val() !='anual'){
+            updateLabel(label,selector.val());
+          }
+        }
+      });
     });
-  });
-  function updateLabel(label, text) {
-    label.text(getPaymentsLabel(text));
-  }
-  function getPaymentsLabel(period){
-    if (period == 'mensual') {
-      return 'Primas Recibos Subs:(11) ';
+    function updateLabel(label, text) {
+      label.text(getPaymentsLabel(text));
     }
-    else if(period == 'trimestral') {
-      return 'Primas Recibos Subs:(3) ';
+    function getPaymentsLabel(period){
+      if (period == 'mensual') {
+        return 'Primas Recibos Subs:(11) ';
+      }
+      else if(period == 'trimestral') {
+        return 'Primas Recibos Subs:(3) ';
+      }
+      else if(period == 'semestral') {
+        return 'Primas Recibos Subs:(1) ';
+      }
     }
-    else if(period == 'semestral') {
-      return 'Primas Recibos Subs:(1) ';
-    }
-  }
-})(jQuery);
+  })(jQuery);
