9a35ebc05ecf8611ae2215f528931010b29e5cbd
jcastro <jcastro@d4all.mx>
2017-11-22 20:21:59 -0700

    bug/386: Added target_blank to a link contrato on a new sinister.

--- sites/all/modules/custom/sinister/js/siniestro.reserva.js
+++ sites/all/modules/custom/sinister/js/siniestro.reserva.js
@@ -1,24 +1,30 @@
 (function ($) {
-    $(document).on('ready', function(){
-      var group =  $('.group-reserva input');
-			var reservatotal = $('#edit-field-reserva-total input');
-			var total = 0;
-			var linkcontrato = jQuery('label[for="edit-field-poliza-und-0"] > span > a');
-			
-			linkcontrato.attr("target", "_blank");
-			
-			group.change(function(){
-				var suma = 0
-				group.each(function(){
-					if(this.value === ""){
-						this.value = 0;
-				  }
-				    total = parseInt(total + parseInt(this.value));
-				    console.log(total);
-			  })
-			   reservatotal[0].value = total - reservatotal[0].value;
-			   total = 0;
-			})
-		 
-    });
-  })(jQuery);
+  $(document).on('ready', function(){
+	  var group =  $('.group-reserva input');
+		var reservatotal = $('#edit-field-reserva-total input');
+		var total = 0;
+
+		group.change(function(){
+			var suma = 0
+			group.each(function(){
+				if(this.value === ""){
+					this.value = 0;
+			  }
+			    total = parseInt(total + parseInt(this.value));
+			    console.log(total);
+		  })
+		   reservatotal[0].value = total - reservatotal[0].value;
+		   total = 0;
+		})
+
+	});
+	
+	$(document).on('change', function(){
+		var linkcontrato = $('label[for="edit-field-poliza-und-0"] > span > a');
+	  var checkcontrato = $('#edit-field-poliza-und-0');
+	  var valuescontrato = $('#field-poliza-values .odd'); 
+
+	  linkcontrato.attr("target", "_blank");
+
+	});
+})(jQuery);
