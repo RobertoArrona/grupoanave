e888596db159fcf0409eb73055014d4251b4f418
Jorge Leonel Sánchez Fregozo <jsanchez@JMacBook-Pro.local>
2017-12-30 14:07:48 -0700

    Added font styles to main menu.

--- sites/all/themes/grupoanave/js/search-home.js
+++ sites/all/themes/grupoanave/js/search-home.js
@@ -2,5 +2,6 @@
 $( document ).ready(function() {
     $('.home-search div span').text('');
     $(".navbar-nav").attr("id","mymenu");
+    $(".home-search div span").css("transform","scaleY(1)");
   })
 })(jQuery);
