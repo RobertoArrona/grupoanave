8204b7ed7d6f5a8c3facf1a13c3b774ad9d5bc41
jcastro <jcastro@d4all.mx>
2018-02-02 17:11:44 -0700

    fix/pago referenciado title name: Changed order of js code execution on referenced receip payment form.

--- sites/all/modules/custom/insurance_contract/js/contratos.pagos.js
+++ sites/all/modules/custom/insurance_contract/js/contratos.pagos.js
@@ -190,4 +190,19 @@
   function hideCreateReceiptOption() {
     $('.action-links').css('display', 'none');
   }
+  
+  var contractFolio = $('.node-collection-navigation-wrapper .node-collection-menu-level-1 .menu li.first.leaf a').attr('href');
+      contractFolio = contractFolio.split('/');
+      contractFolio = contractFolio[2];
+      contractFolio = contractFolio.split('-');
+      contractFolio = contractFolio[0];
+      contractFolio = contractFolio.toUpperCase();
+      var contract = $('.node-collection-navigation-wrapper .node-collection-menu-level-1 .menu li.first.leaf a');
+      contract.text('Contrato-' + contractFolio)
+      var receipt = $(".node-collection-navigation-wrapper .node-collection-menu-level-1 .menu li:not('.first') a");
+      receipt.each(function(){
+        var receiptFolio = $(this).text();
+        $(this).text('Recibo-'+receiptFolio);
+      });
+      
 })(jQuery);
