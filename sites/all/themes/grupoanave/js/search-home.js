48c00755e4c517aad0a7366a7d47b6a2e490f556
cristian100 <cristian100@gmail.com>
2018-02-23 14:24:14 -0700

    Fixed markup for phpcs on js file.

--- sites/all/themes/grupoanave/js/search-home.js
+++ sites/all/themes/grupoanave/js/search-home.js
@@ -1,5 +1,10 @@
+/**
+ * @file
+ * Prepares Search Form markup.
+ */
+
 (function ($) {
-$( document ).ready(function() {
+  $(document).ready(function () {
     $('.home-search div span').text('');
     $(".navbar-nav").attr("id","mymenu");
     $(".home-search div span").css("transform","scaleY(1)");
