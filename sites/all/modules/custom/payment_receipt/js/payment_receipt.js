5166a0496baf3c63b7a8ac4d0bd11586531cb991
jcastro <jcastro@d4all.mx>
2018-02-26 12:03:03 -0700

    feature/payment_receipt: Cleaned code for payment_receipt module.

--- sites/all/modules/custom/payment_receipt/js/payment_receipt.js
+++ sites/all/modules/custom/payment_receipt/js/payment_receipt.js
@@ -1,5 +1,13 @@
+/**
+ * @file
+ * This has data manipulation functions.
+ */
+
+/**
+ * JS Recibo contrato.
+ */
 (function ($) {
-  $( document ).ready(function() {
+  $(document).ready(function () {
     var contractFolio = $('.node-collection-navigation-wrapper .node-collection-menu-level-1 .menu li.first.leaf a').attr('href');
     if (contractFolio) {
       contractFolio = contractFolio.split('/');
@@ -10,9 +18,9 @@
       var contract = $('.node-collection-navigation-wrapper .node-collection-menu-level-1 .menu li.first.leaf a');
       contract.text('Contrato-' + contractFolio)
       var receipt = $(".node-collection-navigation-wrapper .node-collection-menu-level-1 .menu li:not('.first') a");
-      receipt.each(function(){
+      receipt.each(function () {
       var receiptFolio = $(this).text();
-        $(this).text('Recibo-'+receiptFolio);
+        $(this).text('Recibo-' + receiptFolio);
       });
     }
   });
