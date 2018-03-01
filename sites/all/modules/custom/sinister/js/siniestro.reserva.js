b0da300b4a4279c3c0d542fc6c5e4b4c42d1dc5d
Leonel <jlsanchezfr@gmail.com>
2018-02-28 22:33:13 -0700

    feature/update-webservice: Added update webservices.

--- sites/all/modules/custom/sinister/js/siniestro.reserva.js
+++ sites/all/modules/custom/sinister/js/siniestro.reserva.js
@@ -1,35 +1,38 @@
+/**
+ * @file
+ * Retrieves contract values.
+ */
+
 (function ($) {
-  $(document).on('ready', function(){
-	  var group =  $('.group-reserva input');
-		var reservatotal = $('#edit-field-reserva-total input');
-		var total = 0;
+  $(document).on('ready', function () {
+    var group = $('.group-reserva input');
+    var reservatotal = $('#edit-field-reserva-total input');
+    var total = 0;
 
-		group.change(function(){
-			var suma = 0
-			group.each(function(){
-				if(this.value === ""){
-					this.value = 0;
-			  }
-			    total = parseInt(total + parseInt(this.value));
-			    console.log(total);
-		  })
-		   reservatotal[0].value = total - reservatotal[0].value;
-		   total = 0;
-		});
-		
-		$('#edit-field-conductor-und-0-field-conductor-es-quien-reporta-und').on('change', function(){
-  		$('#edit-field-conductor-und-0-field-nombre-de-quien-reporta, #edit-field-conductor-und-0-field-tel-fono').toggleClass('displayblock');
-  		console.log('change event');
-		});
+    group.change(function () {
+      var suma = 0;
+      group.each(function () {
+        if (this.value === "") {
+          this.value = 0;
+        }
+        total = parseInt(total + parseInt(this.value));
+        console.log(total);
+      })
+      reservatotal[0].value = total - reservatotal[0].value;
+      total = 0;
+    });
 
-	});
-	
-	$(document).on('change', function(){
-		var linkcontrato = $('label[for="edit-field-poliza-und-0"] > span > a');
-	  var checkcontrato = $('#edit-field-poliza-und-0');
-	  var valuescontrato = $('#field-poliza-values .odd'); 
+    $('#edit-field-conductor-und-0-field-conductor-es-quien-reporta-und').on('change', function () {
+      $('#edit-field-conductor-und-0-field-nombre-de-quien-reporta, #edit-field-conductor-und-0-field-tel-fono').toggleClass('displayblock');
+      console.log('change event');
+    });
+  });
 
-	  linkcontrato.attr("target", "_blank");
+  $(document).on('change', function () {
+    var linkcontrato = $('label[for="edit-field-poliza-und-0"] > span > a');
+    var checkcontrato = $('#edit-field-poliza-und-0');
+    var valuescontrato = $('#field-poliza-values .odd');
 
-	});
+    linkcontrato.attr("target", "_blank");
+  });
 })(jQuery);
