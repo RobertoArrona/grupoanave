ebe93daf922083430b472fbb02e71e23e0fcfe85
Abraham Preciado Fonseca <preciado0425@gmail.com>
2019-07-27 19:48:27 -0600

    feature/sinister-view-filters: Modified labels on view filters for sinister view and payment receipt view.

--- sites/all/modules/custom/sinister/js/date-picker.js
+++ sites/all/modules/custom/sinister/js/date-picker.js
@@ -6,8 +6,25 @@
 (function ($) {
 
   $(document).on('ready', function () {
+    if (!$('body').hasClass('page-admin-anave-siniestros')) {
+      return false;
+    }
+
     // Setup date picker.
     $('.views-widget-filter-created .form-item #edit-created-min, .views-widget-filter-created .form-item #edit-created-max').datepicker();
+
+    // Setup date labels.
+    sinister_add_date_labels();
   });
+
+  /**
+   * Adds date labels.
+   */
+  function sinister_add_date_labels() {
+    // Add "Desde" label on created min field.
+    $('.form-type-textfield.form-item-created-min').prepend('<label for"edit-created-min">Desde</label>');
+    // Modify label on created max field.
+    $('.form-type-textfield.form-item-created-max').find('> label').text('Hasta');
+  }
 
 })(jQuery);
