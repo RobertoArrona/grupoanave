fd2a4d8d9a3a072bd5e27263484ef8d97a0e766a
Abraham Preciado Fonseca <preciado0425@gmail.com>
2019-08-09 13:10:17 -0600

    feature/views-data-export: Renamed function to add go back link on views data export.

--- sites/all/modules/custom/insurance_contract/js/contract.view.data.export.js
+++ sites/all/modules/custom/insurance_contract/js/contract.view.data.export.js
@@ -8,26 +8,26 @@
     // Add go back link on insurance contract view data export.
     if ($('body').hasClass('page-admin-global-contratosxls')) {
       var url = '/admin/global/contratos';
-      insurance_contract_go_back_link(url);
+      insurance_contract_add_go_back_link(url);
     }
 
     // Add go back link on sinister view data export.
     if ($('body').hasClass('page-admin-global-datos-de-siniestroxls')) {
       var url = '/admin/global/siniestros';
-      insurance_contract_go_back_link(url);
+      insurance_contract_add_go_back_link(url);
     }
 
     // Add go back link on payment receipt view data export.
     if ($('body').hasClass('page-admin-global-recibos-de-contratosxls')) {
       var url = '/admin/global/recibos';
-      insurance_contract_go_back_link(url);
+      insurance_contract_add_go_back_link(url);
     }
   });
 
   /**
    * Adds go back link.
    */
-  function insurance_contract_go_back_link(url) {
+  function insurance_contract_add_go_back_link(url) {
     // Create link.
     var link = '<p><a href="' + url + '">Regresar</a></p>';
 
